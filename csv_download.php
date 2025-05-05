854d07aa115ad00367d8d0cb545a12c979b50123
Michael schrijvers <michael.10330@hotmail.com>
2025-05-05 20:00:19 +0200

    ignore

--- csv_download.php
+++ csv_download.php
@@ -2,11 +2,9 @@
 session_start();
 if (isset($_SESSION['parsedData'])) {
     $parsedDataArray = $_SESSION['parsedData'];
-    // Set headers to force CSV download
     header('Content-Type: text/csv');
     header('Content-Disposition: attachment; filename="parsed_data.csv"');
 
-    // Open PHP output stream
     $output = fopen('php://output', 'w');
 
     fputcsv($output, ['id', 'date', 'name', 'VAT', 'tax percentage', 'total excl tax', 'total tax']);
